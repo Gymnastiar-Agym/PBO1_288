92a12d0e627bc169db82eefadbe8d9f0ddb5df18
Gymnastiar-Agym <83742709+Gymnastiar-Agym@users.noreply.github.com>
2024-03-29 08:19:42 +0700

    Create TextBook.java

---
+++ src/main/java/org/example/TextBook.java
@@ -0,0 +1,6 @@
+// Kelas TextBook sebagai subclass dari Book
+public class TextBook extends Book {
+    public TextBook(String bookId, String title, String author, String category, int stock) {
+        super(bookId, title, author, category, stock);
+    }
+}
