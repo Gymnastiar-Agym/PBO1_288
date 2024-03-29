f8e32e9e5167cf856ea8055028d84a37077852a4
Gymnastiar-Agym <83742709+Gymnastiar-Agym@users.noreply.github.com>
2024-03-29 08:18:41 +0700

    Create StoryBook.java

---
+++ src/main/java/org/example/StoryBook.java
@@ -0,0 +1,6 @@
+// Kelas StoryBook sebagai subclass dari Book
+public class StoryBook extends Book {
+    public StoryBook(String bookId, String title, String author, String category, int stock) {
+        super(bookId, title, author, category, stock);
+    }
+}
