714a56239d522ac45b1f24be1a6ff533c8a1fa44
Gymnastiar-Agym <83742709+Gymnastiar-Agym@users.noreply.github.com>
2024-03-29 08:17:41 +0700

    Create Historybook.java

---
+++ src/main/java/org/example/Historybook.java
@@ -0,0 +1,6 @@
+// Kelas HistoryBook sebagai subclass dari Book
+public class HistoryBook extends Book {
+    public HistoryBook(String bookId, String title, String author, String category, int stock) {
+        super(bookId, title, author, category, stock);
+    }
+}
